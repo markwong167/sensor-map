2dd4c2223cabe00d991a3aee1e2669301c9e2d13
Mark <markwong167@gmail.com>
2026-04-15 12:11:11 -0400

    added sensors

--- app/PinchMap.tsx
+++ app/PinchMap.tsx
@@ -1,9 +1,18 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 export const PinchMap = () => {
+  const sensors = [
+    { id: 1, label: "sensor 1", left: 100, top: 200, height: 1 },
+    { id: 2, label: "sensor 2", left: 600, top: 500, height: 3 },
+    { id: 3, label: "sensor 3", left: 1800, top: 300, height: 0.51 },
+    { id: 4, label: "sensor 4", left: 500, top: 1200, height: 6 },
+  ];
+  const imageHeight = 1647;
+  const imageWidth = 2059;
   return (
     <TransformWrapper>
       <TransformComponent>
@@ -14,6 +23,21 @@ export const PinchMap = () => {
           height={1647}
           priority
         />
+        {sensors.map((sensor) => (
+          <Button
+            key={sensor.id}
+            type='button'
+            onClick={() => console.log("clicked", sensor.id)}
+            className='absolute z-10 -translate-0.5'
+            style={{
+              left: `${(sensor.left / imageWidth) * 100}%`,
+              top: `${(sensor.top / imageHeight) * 100}%`,
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            {sensor.label}
+          </Button>
+        ))}
       </TransformComponent>
     </TransformWrapper>
   );
