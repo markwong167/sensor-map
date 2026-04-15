e47e384f719cb81b08d8de312da20c07aa78d916
Mark <markwong167@gmail.com>
2026-04-15 10:57:40 -0400

    added info panel

---
+++ app/InfoPanel.tsx
@@ -0,0 +1,10 @@
+export const InfoPanel = () => {
+  return (
+    <div className='flex flex-col h-full align-middle'>
+      <div className='bg-blue-900 h-20'>
+        <h1 className='text-3xl text-center mt-4'>Info Panel</h1>
+      </div>
+      <div className='h-dvh-1/2'></div>
+    </div>
+  );
+};
